611c5b59efbdcdc9d4524a814897b50947eb355a
luiis <luiis.educa@gmail.com>
2022-05-12 20:34:15 -0500

    tle:5,1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             // 'throttle:api',
-            'throttle:60,1',
+            'throttle:5,1',
             // \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
